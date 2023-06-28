195e2088f30d2d5f3c5781191f957645812709e5
Diogoesteves11 <a1004004@alunos.uminho.pt>
2023-06-28 14:06:29 +0100

    more changes

--- codigo/main.c
+++ codigo/main.c
@@ -289,11 +289,13 @@ void draw_explosion (int x, int y, MAPA *map, MONSTERS *monsters, int *num_enemi
       }
       else if (testch == 'x'){
         draw_explosion(ix,iy,map,monsters,num_enemies);
+        map->matrix[ix][iy] = '^';
       }
       else if(testch == '&'){
         for (int i = 0; i < (*num_enemies); i++){
           if(monsters[i].x == ix&& monsters[i].y == iy){
            kill_monster(ix,i,iy);
+           map->matrix[ix][iy] = '^';
           } 
         }
       }
